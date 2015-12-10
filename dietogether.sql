e558ecfa8d42a31e2512a59c261dd5999c031771
ghykj <ghykj13@gmail.com>
2015-12-10 17:50:02 +0900

    update sql

--- dietogether.sql
+++ dietogether.sql
@@ -18,3 +18,21 @@ CREATE TABLE `dietogether`.`friendship` (
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE `health` (
+  `idx` int(11) NOT NULL AUTO_INCREMENT,
+  `id` varchar(50) NOT NULL,
+  `date` varchar(50) NOT NULL,
+  `health_name` varchar(45) NOT NULL,
+  `health_count` int(11) NOT NULL,
+  PRIMARY KEY (`idx`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `walking` (
+  `index` int(11) NOT NULL AUTO_INCREMENT,
+  `id` varchar(50) DEFAULT NULL,
+  `date` varchar(50) DEFAULT NULL,
+  `walking_count` int(11) DEFAULT NULL,
+  `calorie` int(11) DEFAULT NULL,
+  PRIMARY KEY (`index`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
